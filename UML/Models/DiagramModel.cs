b7ecabc1123d7a0c8bfbf8a02eaaec79bdf43789
Trevor Foresta <trevforesta@gmail.com>
2022-09-14 18:08:45 -0400

    Readme + Minor HTML/CSS changes
    Added instructional readme and moved login to the right side
    
    Also changed DiagramModel as per Galen's suggestion

--- UML/Models/DiagramModel.cs
+++ UML/Models/DiagramModel.cs
@@ -5,7 +5,8 @@ namespace UML.Models
     public class DiagramModel
     {
         [BsonId]
-        public string _id { get; set; }
+        public ObjectId Id { get; set; }
+
         public string Username { get; set; }
         public ScreenModel[] screen { get; set; }
     }
