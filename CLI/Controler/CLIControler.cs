31414b9525f439ac33b204e669390038ab1ce92f
Alfather-Bear <nordic-bear@outlook.com>
2022-10-01 00:37:10 -0400

    For Logan

--- CLI/Controler/CLIControler.cs
+++ CLI/Controler/CLIControler.cs
@@ -20,6 +20,27 @@ namespace CLI.Controllers
 {
     public class CLIController : Controller
     {
-
+        public static void interpet(Commands input)
+        {
+            if(input == Commands.help)
+            {
+                Console.WriteLine("List of Commands:");
+                Console.WriteLine("Help: Displays a list of commands with brief explanations");
+                Console.WriteLine("Add_Class: Creates a new class if name is valid. Input an name after the promt");
+                Console.WriteLine("Add_field: Add a field to a class. Input the class name after the promt, the input name");
+                Console.WriteLine("And type after their promt");
+                Console.WriteLine("Add_Meth: Add a method to a class. Input the class name after the promt, the input name");
+                Console.WriteLine("And return type");
+                Console.WriteLine("Rem_Class:");
+                Console.WriteLine("Rem_field:");
+                Console.WriteLine("Rem_Meth:");
+                Console.WriteLine("relat:");
+                Console.WriteLine("Rem_relat:");
+                Console.WriteLine("save:");
+                Console.WriteLine("load:");
+                Console.WriteLine("import:");
+                Console.WriteLine("export:");
+            }
+        }
     }
 }
---
+++ CLI/Models/CommandEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace CLI.Models
+{
+    public enum Commands
+    {
+        exit,
+        add_class,
+        add_field,
+        add_meth,
+        rem_class,
+        rem_attr,
+        rem_meth,
+        relat,
+        rem_relat,
+        save,
+        load,
+        import,
+        export,
+        help
+    }
+}
