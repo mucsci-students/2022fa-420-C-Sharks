e2b8ba3eebdd19ea1256d9131d8b54190367bd97
Alfather-Bear <nordic-bear@outlook.com>
2022-10-23 01:02:24 -0400

    Auto Complete

--- CLI/Views/CLIView.cs
+++ CLI/Views/CLIView.cs
@@ -1,23 +1,67 @@
 ﻿using CLI.Models;
 using CLI.Controllers;
-Console.WriteLine("WELCOME TO C-SHARK UML COMMAND LINE INTERFACE");
+using AutoCompleteUtils;
+using ConsoleUtils;
+//using AutoCompletUtils;
+//Globals
+List<string> CommandLST = new List<string>
+{
+        "exit",
+        "add_class",
+        "add_field",
+        "add_meth",
+        "rem_class",
+        "rem_field",
+        "rem_meth",
+        "relat",
+        "rem_relat",
+        "list_class",
+        "list_classes",
+        "list_relat",
+        "import",
+        "export",
+        "help",
+        "print",
+        "mod_class",
+        "mod_field",
+        "mod_meth",
+        "mod_relat",
+};
+var curDat = DateTime.Now;
+Console.WriteLine($"WELCOME TO C-SHARK UML COMMAND LINE INTERFACE {curDat:d}");
 bool exitCon = false;
-string UsrIpt;
+string UsrIpt = "";
+Console.Title = "C-Sharks";
 Commands Input = new Commands();
+string autocom;
+var cyclingAutoComplete = new CyclingAutoComplete();
+Console.WriteLine("PLEASE ENTER A COMMAND:");
 while (exitCon == false)
 {
-    Console.WriteLine("PLEASE ENTER A COMMAND:");
-    UsrIpt = Console.ReadLine();
-    if (Enum.TryParse<Commands>(UsrIpt, true, out Input))
+   
+    var result = ConsoleExt.ReadKey();
+    switch (result.Key)
     {
-        if (Input == Commands.exit)
-        {
-            exitCon = true;
-        }
-        CLIController.interpet(Input);
+        case ConsoleKey.Enter:
+            UsrIpt = result.LineBeforeKeyPress.Line;
+            if (Enum.TryParse<Commands>(UsrIpt, true, out Input))
+            {
+                if (Input == Commands.exit)
+                {
+                    exitCon = true;
+                }
+                CLIController.interpet(Input);
+            }
+            else
+            {
+                Console.WriteLine("INVALID COMMAND TRY HELP:");
+            }
+            break;
+        case ConsoleKey.Tab:
+            autocom = cyclingAutoComplete.AutoComplete(result.LineBeforeKeyPress.LineBeforeCursor,CommandLST);
+            ConsoleExt.SetLine(autocom);
+            break;
     }
-    else
-    {
-        Console.WriteLine("INVALID COMMAND TRY HELP:");
-    }
+    //UsrIpt = Console.ReadLine();
+    
 }
